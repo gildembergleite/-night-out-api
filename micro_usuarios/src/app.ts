b4bc5ccf3d50c610dda39c163c256ea6c5f1418c
Carlos Eduardo <eduardorodrigues@gmail.com>
2025-11-12 09:51:01 -0300

    adicão de cors

--- micro_usuarios/src/app.ts
+++ micro_usuarios/src/app.ts
@@ -6,13 +6,19 @@ import artistaRoutes from "./web/routes/artista/artistaRoutes";
 import AuthRoutes from "./web/routes/auth/authRoutes";
 import casaDeShowRoutes from "./web/routes/casa_de_show/casaDeShowRoutes";
 import clienteRoutes from "./web/routes/cliente/clienteRoutes";
+import { errorHandler } from "./core/middleware/validacao";
 
 const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5174",
+      "http://127.0.0.1:5174",
+      "http://localhost:3000",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    optionsSuccessStatus: 200,
     credentials: true,
   })
 );
@@ -24,6 +30,8 @@ app.use("/adm", AdmRoutes);
 app.use("/cliente", clienteRoutes);
 app.use("/casaDeShow", casaDeShowRoutes);
 app.use("/artista", artistaRoutes);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 
